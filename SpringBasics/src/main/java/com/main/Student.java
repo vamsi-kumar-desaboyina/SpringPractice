d8d97c68b681744c5f29111b13b73f2a86686e77
vamsi-kumar-desaboyina <desaboyinavamsi@gmail.com>
2026-02-18 16:34:22 +0530

    one

---
+++ SpringBasics/src/main/java/com/main/Student.java
@@ -0,0 +1,31 @@
+package com.main;
+
+public class Student {
+	int number;
+	String name;
+	public Student()
+	{
+		System.out.println("Hello");
+	}
+	public Student(int number, String name) {
+		super();
+		this.number = number;
+		this.name = name;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Student [number=" + number + ", name=" + name + "]";
+	}
+}
